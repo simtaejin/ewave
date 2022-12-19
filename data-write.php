2cda69a5d394f7c59c31177b32c9027ae6f5f79a
simtaejin <stj53@nate.com>
2022-12-19 12:02:14 +0900

    데이터 받는 파일일 변경

--- data-write.php
+++ data-write.php
@@ -5,10 +5,11 @@ foreach ($_REQUEST as $k => $v) {
     $$k = $v;
 }
 
+$date = date("Y-m-d");
+$time = date("H:i:s");
+$create_at = date("Y-m-d H:i:s");
+
 if ($type=='typeA') {
-    $date = date("Y-m-d");
-    $time = date("H:i:s");
-    $create_at = date("Y-m-d H:i:s");
 
     $sql = "INSERT INTO nodeA (`gid`, `a_nid`, `date`, `time`, `date_1`, `date_2`, `date_3`, `date_4`, `date_5`, `date_6`, `date_7`, `date_8`, `create_at`)
     VALUES ($gid, $nid, '$date', '$time',$date_1,$date_2,$date_3,$date_4,$date_5,$date_6,$date_7,$date_8, '{$create_at}')";
@@ -17,5 +18,7 @@ if ($type=='typeA') {
 
     $sql = "INSERT INTO nodeB (`gid`, `b_nid`, `date`, `time`, `date_1`, `date_2`, `date_3`, `date_4`, `date_5`, `date_6`, `control_7`, `control_8`, `create_at`)
     VALUES ($gid, $nid, '$date', '$time',$date_1,$date_2,$date_3,$date_4,$date_5,$date_6,$control_7,$control_8, '{$create_at}')";
+
+echo $sql;exit;
     $result = mysqli_query($conn, $sql);
 }
